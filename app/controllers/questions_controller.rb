c059a491612c0f13eb0807be8801782aa5e6f4f5
antoine broume <antoine.broume@hotmail.fr>
2021-03-01 16:47:58 +0100

    Ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+
+class QuestionsController < ApplicationController
+
+
+    def ask 
+    end
+    def answer 
+     @ask = params[:question]
+     @answer = coach_answer(@ask)
+    end
+
+    def coach_answer(your_message)
+
+    if your_message.downcase == "i am going to work"
+      return "Great"
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
